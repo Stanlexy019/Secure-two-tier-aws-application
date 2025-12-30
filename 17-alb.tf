e58bb87d70c4d25849b1f468af1ea32e6379593b
Stanlexy019 <stanleyokoli36@gmail.com>
2025-12-30 13:51:08 +0100

    Finally built a two tier AWS infra secured ans scalable

--- 17-alb.tf
+++ 17-alb.tf
@@ -1,5 +1,5 @@
 resource "aws_lb" "app_alb" {
-  name               = "${var.terraform_vpc}-alb"
+  name               = "stanley-vpc-alb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
@@ -10,6 +10,6 @@ resource "aws_lb" "app_alb" {
   ]
 
   tags = {
-    Name = "${var.terraform_vpc}-alb"
+    Name = "stanley-vpc-alb"
   }
 }
