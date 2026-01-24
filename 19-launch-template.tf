9c1f4d1560ba7fde969437c7b11c0487c4f28c28
Stanlexy019 <stanleyokoli36@gmail.com>
2026-01-24 09:37:56 +0100

    added IAM roles for ec2 and added ssm secrets for my application

--- 19-launch-template.tf
+++ 19-launch-template.tf
@@ -10,18 +10,53 @@ resource "aws_launch_template" "app_lt" {
   user_data = base64encode(<<-EOF
     #!/bin/bash
     apt-get update -y
-    apt-get install -y nginx
-    systemctl start nginx
-    systemctl enable nginx
-    echo "Hello from Auto Scaling EC2" > /var/www/html/index.html
+    apt-get install -y curl unzip
+    snap install aws-cli --classic
+
+
+# Retrieve secrets from AWS Systems Manager Parameter Store
+JWT_SECRET=$(aws ssm get-parameter \
+  --name "/app/prod/jwt_secret" \
+  --with-decryption \
+  --query "Parameter.Value" \
+  --output text \
+  --region eu-north-1)
+
+MONGO_URI=$(aws ssm get-parameter \
+  --name "/app/prod/mongo_uri" \
+  --with-decryption \
+  --query "Parameter.Value" \
+  --output text \
+  --region eu-north-1)
+
+# Export as environment variables
+export JWT_SECRET=$JWT_SECRET
+export MONGO_URI=$MONGO_URI
+
+# Install and configure services
+apt-get install -y nginx docker.io
+systemctl start nginx
+systemctl enable nginx
+systemctl start docker
+systemctl enable docker
+
+# Install Docker Compose
+curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# Create default web page
+echo "Hello from Auto Scaling EC2" > /var/www/html/index.html
   EOF
   )
 
   tag_specifications {
     resource_type = "instance"
-
     tags = {
       Name = "autoscaling-app"
     }
+  }
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ec2_instance_profile.name
   }
 }
